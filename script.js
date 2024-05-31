b75f2bc2b6fccf27468cb93ee61d8ae418578051
s-jaswal <sjasdocs@gmail.com>
2024-05-31 18:06:39 -0400

    images aren't showing up :( life hates me

--- script.js
+++ script.js
@@ -1,130 +1,130 @@
 const teamMembers = [
-  {
-      name: 'The Brain',
-      imageURL: 'brain.jpg',
-      activelyPlaying: true,
-      type: 'Color Fairy',
-      skills: ['Using magic', 'Being joyful', 'Playing with Rachel & Kristy'],
-      strengths: 'Creating the color Orange',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Amber is a cheerful fairy who loves to spread joy and color.'
-  },
-  {
-      name: 'The Heart',
-      imageURL: 'heart.webp', 
-      type: 'Animal Fairy',
-      skills: ['Communicating with animals', 'Cat whispering', 'Playing with Rachel & Kristy'],
-      strengths: 'Animals',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Katie is a fairy who loves kittens and has a special bond with them.'
-  },
-  {
-      name: 'The Lungs',
-      imageURL: 'lungs.jpg', 
-      type: 'Color Fairy',
-      skills: ['Spreading happiness', 'Coloring the world red', 'Playing with Rachel & Kristy'],
-strengths: 'Bringing joy and color',
-weaknesses: 'Jack Frost & his Goblins',
-biography: 'Ruby is a fairy who loves the color red and has the magical ability to spread happiness wherever she goes.'
-},
-  {
-      name: 'The Liver',
-      imageURL: 'liver.jpg', 
-      type: 'Flower Fairy',
-      skills: ['Tending to poppies', 'Bringing color to gardens', 'Playing with Rachel & Kristy'],
-      strengths: 'Flowers and gardening',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Pippa is a fairy who loves poppies and has a special bond with them, bringing vibrant color to every garden she visits.'
+    {
+        name: 'The Brain',
+        imageURL: 'brain.jpg',
+        activelyPlaying: true,
+        type: 'Color Fairy',
+        skills: ['Using magic', 'Being joyful', 'Playing with Rachel & Kristy'],
+        strengths: 'Creating the color Orange',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Amber is a cheerful fairy who loves to spread joy and color.'
     },
-  {
-      name: 'The Kidneys',
-      imageURL: 'kidneys.jpg', 
-      type: 'Talent Fairy',
-      skills: ['Baking delicious treats', 'Decorating cakes', 'Playing with Rachel & Kristy'],
-      strengths: 'Baking and confectionery',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Roxie is a fairy who loves baking and has a special talent for creating the most delightful treats, spreading joy with her delicious creations.'
+    {
+        name: 'The Heart',
+        imageURL: 'heart.webp',
+        type: 'Animal Fairy',
+        skills: ['Communicating with animals', 'Cat whispering', 'Playing with Rachel & Kristy'],
+        strengths: 'Animals',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Katie is a fairy who loves kittens and has a special bond with them.'
     },
-  {
-      name: 'The Pancreas',
-      imageURL: 'pancreas.jpg', 
-      type: 'Music Fairy',
-      skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
-      strengths: 'Music and creativity',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    {
+        name: 'The Lungs',
+        imageURL: 'lungs.jpg',
+        type: 'Color Fairy',
+        skills: ['Spreading happiness', 'Coloring the world red', 'Playing with Rachel & Kristy'],
+        strengths: 'Bringing joy and color',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ruby is a fairy who loves the color red and has the magical ability to spread happiness wherever she goes.'
     },
-  {
-      name: 'The Skin',
-      imageURL: 'skin.jpg',
-      type: 'Music Fairy',
-      skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
-      strengths: 'Music and creativity',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    {
+        name: 'The Liver',
+        imageURL: 'liver.jpg',
+        type: 'Flower Fairy',
+        skills: ['Tending to poppies', 'Bringing color to gardens', 'Playing with Rachel & Kristy'],
+        strengths: 'Flowers and gardening',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Pippa is a fairy who loves poppies and has a special bond with them, bringing vibrant color to every garden she visits.'
     },
-  {
-      name: 'The Stomach',
-      imageURL: 'stomach.avif', 
-      type: 'Music Fairy',
-      skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
-      strengths: 'Music and creativity',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    {
+        name: 'The Kidneys',
+        imageURL: 'kidneys.jpg',
+        type: 'Talent Fairy',
+        skills: ['Baking delicious treats', 'Decorating cakes', 'Playing with Rachel & Kristy'],
+        strengths: 'Baking and confectionery',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Roxie is a fairy who loves baking and has a special talent for creating the most delightful treats, spreading joy with her delicious creations.'
     },
-  {
-      name: 'The Bone Marrow',
-      imageURL: 'bone marrow.jpg', 
-      type: 'Music Fairy',
-      skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
-      strengths: 'Music and creativity',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    {
+        name: 'The Pancreas',
+        imageURL: 'pancreas.jpg',
+        type: 'Music Fairy',
+        skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
+        strengths: 'Music and creativity',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
     },
-  {
-      name: 'The Veins',
-      imageURL: 'veins.webp', 
-      type: 'Music Fairy',
-      skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
-      strengths: 'Music and creativity',
-      weaknesses: 'Jack Frost & his Goblins',
-      biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    {
+        name: 'The Skin',
+        imageURL: 'skin.jpg',
+        type: 'Music Fairy',
+        skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
+        strengths: 'Music and creativity',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    },
+    {
+        name: 'The Stomach',
+        imageURL: 'stomach.avif',
+        type: 'Music Fairy',
+        skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
+        strengths: 'Music and creativity',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    },
+    {
+        name: 'The Bone Marrow',
+        imageURL: 'bone marrow.jpg',
+        type: 'Music Fairy',
+        skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
+        strengths: 'Music and creativity',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
+    },
+    {
+        name: 'The Veins',
+        imageURL: 'veins.webp',
+        type: 'Music Fairy',
+        skills: ['Playing the guitar', 'Composing music', 'Playing with Rachel & Kristy'],
+        strengths: 'Music and creativity',
+        weaknesses: 'Jack Frost & his Goblins',
+        biography: 'Ellie is a fairy who loves playing the guitar and has a special talent for composing beautiful music, bringing harmony and joy wherever she goes.'
     },
 ];
 
 function generateTeamCards() {
-  const teamCardsContainer = document.getElementById('teamCards');
+    const teamCardsContainer = document.getElementById('teamCards');
 
-  teamMembers.forEach(member => {
-      const card = document.createElement('div');
-      card.classList.add('col-md-4');
+    teamMembers.forEach(member => {
+        const card = document.createElement('div');
+        card.classList.add('col-md-4');
 
-      // Styling card based on position:
-      let backgroundColor;
+        // Styling card based on position:
+        let backgroundColor;
 
-      switch (member.type.toLowerCase()) {
-          case 'color fairy':
-              backgroundColor = '#ffc107'; // Yellow for color fairy
-              break;
-          case 'animal fairy':
-              backgroundColor = '#28a745'; // Green for animal fairy
-              break;
-          case 'flower fairy':
-              backgroundColor = '#007bff'; // Blue for flower
-              break;
-          case 'talent fairy':
-              backgroundColor = '#dc3545'; // Red for talent
-              break;
-          default:
-              backgroundColor = '#800080'; // purple for other positions
-      }
-      card.style.backgroundColor = backgroundColor;
+        switch (member.type.toLowerCase()) {
+            case 'color fairy':
+                backgroundColor = '#ffc107'; // Yellow for color fairy
+                break;
+            case 'animal fairy':
+                backgroundColor = '#28a745'; // Green for animal fairy
+                break;
+            case 'flower fairy':
+                backgroundColor = '#007bff'; // Blue for flower
+                break;
+            case 'talent fairy':
+                backgroundColor = '#dc3545'; // Red for talent
+                break;
+            default:
+                backgroundColor = '#800080'; // purple for other positions
+        }
+        card.style.backgroundColor = backgroundColor;
 
-      // Create a list of Skills with <li> tags
-      const skillsList = member.skills
-          .map(skill => `<li>${skill}</li>`).join('');
+        // Create a list of Skills with <li> tags
+        const skillsList = member.skills
+            .map(skill => `<li>${skill}</li>`).join('');
 
-      card.innerHTML = `
+        card.innerHTML = `
           <div class="card">
               <img src="${member.imageURL}" class="img-fluid team-member-image" alt="${member.name}">
               <div class="card-header">${member.name}</div>
@@ -142,8 +142,8 @@ function generateTeamCards() {
           </div>
       `;
 
-      teamCardsContainer.appendChild(card);
-  });
+        teamCardsContainer.appendChild(card);
+    });
 }
 
 window.onload = generateTeamCards;
